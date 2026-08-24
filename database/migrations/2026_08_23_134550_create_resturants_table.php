5e6e2de45a1ad372ebfa3988bfefa1811c1b7153
Abdul3Qadir <qadir3legend@gmail.com>
2026-08-24 16:25:46 +0500

    Working... on Database and models

---
+++ database/migrations/2026_08_23_134550_create_resturants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resturants', function (Blueprint $table) {
+
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('restaurant_category_id')->constrained('restaurant_categories');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address');
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->string('logo')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->boolean('is_open')->default(false);
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resturants');
+    }
+};
