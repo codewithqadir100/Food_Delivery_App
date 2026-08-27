9a04d692aaf0cb42dfc29b9cd3a99af7c8f3f980
Abdul3Qadir <qadir3legend@gmail.com>
2026-08-27 20:35:04 +0500

    27 Aug 2026 Restaurant Dashboard

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar(){
             <nav className="flex justify-between items-center px-3 py-2 bg-blue-400">
                 <h3>FoodDeliveryApp</h3>
                 <ul className="flex gap-2">
-                    <li>Login</li>
-                    <li>Admin Login</li>
-                    <li>Restaurant Login</li>
+                    <li><a href={ route('customer.dashboard') }>Customer Login</a></li>
+                    <li><a href={ route('admin.dashboard') }>Admin Login</a></li>
+                    <li><a href={ route('restaurant.dashboard') }>Restaurant Login</a></li>
                 </ul>
             </nav>
     );
